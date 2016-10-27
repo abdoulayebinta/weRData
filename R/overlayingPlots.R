8b52ace85b8becf22a203cf5f8501744063d5bc5
Abdoulaye BAH <Abdoulaye BAH>
2016-10-27 12:38:12 +0000

    Overlaying plots: Histogram, curve, lines and rug

---
+++ R/overlayingPlots.R
@@ -0,0 +1,42 @@
+##############################################
+# Overlaying Plots
+#
+# @author: Abdoulaye Bah
+# Ressource: R Statistics from Lynda.com
+##############################################
+
+# LOAD DATASET
+require("datasets")
+?swiss
+swiss
+str(swiss)
+
+data("swiss")
+fertility <- swiss$Fertility 
+
+# PLOTS
+# Plot 1: Histogram
+h <- hist(fertility,
+          prob = TRUE,
+          ylim = c(0, 0.04),
+          xlim = c(30, 100),
+          breaks = 11,
+          col = "#E5E5E5",
+          border = 0,
+          main = "Fertility for 47 French-Speaking\nSwiss Provinces, c. 1888")
+
+# Plot 2: Normal curve (if prob = TRUE)
+curve(dnorm(x, mean = mean(fertility), sd = sd(fertility)),
+      col = "red",
+      lwd = 3,
+      add = TRUE)
+
+# Plot 3 & 4: Kernel density lines (if prop = TRUE)
+lines(density(fertility), col = "blue")
+lines(density(fertility, adjust = 3), col = "darkgreen")
+
+# Plot 5: Rug (That is, lineplot under histogram)
+rug(fertility, col = "red")
+
+# Clean up
+rm(list = ls())
