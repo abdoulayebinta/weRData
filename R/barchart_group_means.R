5e39dc3e97035a432e5e52836290f5d717de9cb6
Abdoulaye BAH <Abdoulaye BAH>
2016-10-28 15:01:48 +0000

    Add a barplot of mean

--- R/barchart_group_means.R
+++ R/barchart_group_means.R
@@ -12,7 +12,20 @@ means
 
 plot(means)
 
+?t
+# To get a barplot, need to extract means and reorganize 
+# Removes first columns, transposes second
+mean.data <- t(means[-1])
+colnames(mean.data) <- means[,1]
 
+# Basic barplot for means
+barplot(mean.data)
 
+# Modified barplot
+barplot(mean.data,
+        col = "lightblue",
+        main = "Effectiveness of Insect Sparys",
+        xlab = "Spray Used",
+        ylab = "Insect Count")
 # Clean
 rm(list = ls())
